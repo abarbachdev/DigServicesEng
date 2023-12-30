3aa1585d47c2c373e5cb70cb03dab5238370722c
abarbachdev <abarbach.ismail2015@gmail.com>
2023-12-30 23:29:47 +0100

    english website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className='bg-black h-80 border-t   flex-col items-center justify-center'>
     <div className='flex items-center justify-center '>
-            <h1 className='  text-orange-500 text-4xl font-semibold cursor-pointer p-8'> <span className='text-white'>4K</span>  Iptv <span className='text-white'>Box</span> </h1>
+            <h1 className='  text-orange-500 text-4xl font-semibold cursor-pointer p-8'> <span className='text-white'>4K</span>  Iptv <span className='text-white'>USA</span> </h1>
     </div>
     <div className='flex items-center justify-center'>
-    <p className='text-white text-center '>4K Iptv Box is a No.1 IPTV provider in the market, <br/> offers reliable and stable complete IPTV channels <br/>and iptv streaming service.</p>
+    <p className='text-white text-center '>4K Iptv USA is a No.1 IPTV provider in the market, <br/> offers reliable and stable complete IPTV channels <br/>and iptv streaming service.</p>
     </div>
     <div className='flex items-center justify-center text-white p-14'>
     4K IPTV©All Rights Reserved 2019-2023
